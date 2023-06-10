3c2a4c955dd57331b19655a2e06e51f4f3aa02eb
Samir Jadhav <samirzjadhav@gmail.com>
2023-06-10 15:03:37 +0530

    feat: complete todo 6

--- script.js
+++ script.js
@@ -27,16 +27,31 @@ const enable = (button) => button.removeAttribute("disabled");
 // TODO 5: Declare an isCorrect function that compares a guess to the right answer
 // isCorrect(guess) should return true if the guess matches the fact's answer
 function isCorrect(guess) {
-  return guess === fact.answer;
+  return guess === fact.answer.toString();
 }
 
 // TODO 6A: Use a for loop to add a click event listener to each of the optionButtons
-// TODO 6B: Within the event handler function, display the fact's explanation by setting the text of the explanation element
+for (let button of optionButtons) {
+  button.addEventListener("click", (event) => {
+    // TODO 6B: Within the event handler function, display the fact's explanation by setting
+    // the text of the explanation element
+    explanation.textContent = fact.explanation;
 
-// TODO 7: Within the event handler function,
-// Use a for loop to disable all the option buttons
+    // TODO 7: Within the event handler function,
+    // Use a for loop to disable all the option buttons
+    for (let otherButton of optionButtons) {
+      disable(otherButton);
+    }
 
-// TODO 8: Within the event handler function,
-// Get the guessed value from the clicked button
-// Use a conditional to compare the guess to the fact's answer
-// and add the "correct"/"incorrect" class as appropriate
+    console.log(button.value);
+    // TODO 8: Within the event handler function,
+    // Get the guessed value from the clicked button
+    // Use a conditional to compare the guess to the fact's answer
+    // and add the "correct"/"incorrect" class as appropriate
+    if (isCorrect(button.value)) {
+      button.classList.add("correct");
+    } else {
+      button.classList.add("incorrect");
+    }
+  });
+}
